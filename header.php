55a7e47bff2826b319fcb216c9f271edf9eaebf7
newdevdima <elhartichaymaa71@gmail.com>
2026-05-20 15:02:08 +0100

    last version inshae allah

---
+++ header.php
@@ -0,0 +1,54 @@
+<header class="header" data-header>
+    <div class="container">
+
+        <a href="#" class="logo">Brandnova</a>
+
+        <nav class="navbar" data-navbar>
+
+            <div class="wrapper">
+                <a href="#" class="logo">Brandnova</a>
+
+                <button class="nav-close-btn" aria-label="close menu" data-nav-toggler>
+                    <ion-icon name="close-outline" aria-hidden="true"></ion-icon>
+                </button>
+            </div>
+
+            <ul class="navbar-list">
+
+                <li class="navbar-item">
+                    <a href="#home" class="navbar-link" data-nav-link>Home</a>
+                </li>
+
+                <li class="navbar-item">
+                    <a href="#service" class="navbar-link" data-nav-link>Services</a>
+                </li>
+
+                <!-- <li class="navbar-item">
+                  <a href="#feature" class="navbar-link" data-nav-link>Features</a>
+                </li> -->
+
+                <li class="navbar-item">
+                    <a href="#project" class="navbar-link" data-nav-link>Portfolio</a>
+                </li>
+
+                <li class="navbar-item">
+                    <a href="#blog" class="navbar-link" data-nav-link>A propos</a>
+                </li>
+
+                <li class="navbar-item">
+                    <a href="#contact" class="navbar-link" data-nav-link>Contact</a>
+                </li>
+            </ul>
+
+        </nav>
+
+        <button class="nav-open-btn" aria-label="open menu" data-nav-toggler>
+            <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
+        </button>
+
+        <a href="#contact" class="btn btn-primary has-before has-after">Consulter Nous 👋</a>
+
+        <div class="overlay" data-nav-toggler data-overlay></div>
+
+    </div>
+</header>
